f0ac60ed641cb2c22d83bf4dd982b974e8e69aa5
Martin Budden <mjbudden@gmail.com>
2025-01-13 10:55:54 +0000

    Renamed parameter q to v in xyz_type.

--- lib/IMU_TYPES/src/xyz_type.h
+++ lib/IMU_TYPES/src/xyz_type.h
@@ -7,16 +7,16 @@ public:
     inline xyz_t operator+() const { return *this; } //<! Unary plus
     inline xyz_t operator-() const { return xyz_t {-x, -y, -z }; } //<! Unary negation
 
-    inline xyz_t operator+=(const xyz_t& q) { x += q.x; y += q.y; z += q.z; return *this; }
-    inline xyz_t operator-=(const xyz_t& q) { x -= q.x; y -= q.y; z -= q.z; return *this; }
+    inline xyz_t operator+=(const xyz_t& v) { x += v.x; y += v.y; z += v.z; return *this; }
+    inline xyz_t operator-=(const xyz_t& v) { x -= v.x; y -= v.y; z -= v.z; return *this; }
     inline xyz_t operator*=(float k) { x*=k; y*=k; z*=k; return *this; } //<! Multiplication by a scalar
 
     // Binary operations
-    inline xyz_t operator+(const xyz_t& q) const { return xyz_t{x + q.x, y + q.y, z + q.z}; }
-    inline xyz_t operator-(const xyz_t& q) const { return xyz_t{x - q.x, y - q.y, z - q.z}; }
+    inline xyz_t operator+(const xyz_t& v) const { return xyz_t{x + v.x, y + v.y, z + v.z}; }
+    inline xyz_t operator-(const xyz_t& v) const { return xyz_t{x - v.x, y - v.y, z - v.z}; }
     inline xyz_t operator*(float k) const { return xyz_t{x*k, y*k, z*k}; } //<! Multiplication by a scalar
-    inline float dot_product(const xyz_t& q) const { return  x*q.x + y*q.y + z*q.z; } //!< Vector dot product
-    inline xyz_t cross_product(const xyz_t& q) const { return xyz_t{y*q.z - z*q.y, z*q.x - x*q.z, x*q.y - y*q.x}; } //!< Vector cross product
+    inline float dot_product(const xyz_t& v) const { return  x*v.x + y*v.y + z*v.z; } //!< Vector dot product
+    inline xyz_t cross_product(const xyz_t& v) const { return xyz_t{y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x}; } //!< Vector cross product
 public:
     float x;
     float y;
