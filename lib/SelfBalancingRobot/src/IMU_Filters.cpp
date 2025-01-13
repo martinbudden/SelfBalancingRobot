bed4b237f37be8771e873e9fbb91b937cf1344cc
Martin Budden <mjbudden@gmail.com>
2025-01-13 17:58:56 +0000

    Fixed type in IMU_Filters.

--- lib/SelfBalancingRobot/src/IMU_Filters.cpp
+++ lib/SelfBalancingRobot/src/IMU_Filters.cpp
@@ -4,9 +4,9 @@
 void IMU_Filters::filter(xyz_t& gyroRadians, xyz_t& acc, float deltaT) // cppcheck-suppress constParameterReference false positive
 {
     gyroRadians.x = _filterGyroX.update(gyroRadians.x, deltaT);
-    gyroRadians.y = _filterGyroX.update(gyroRadians.y, deltaT);
-    gyroRadians.z = _filterGyroX.update(gyroRadians.z, deltaT);
-    acc.x = _filterAccZ.update(acc.x, deltaT);
-    acc.y = _filterAccZ.update(acc.y, deltaT);
+    gyroRadians.y = _filterGyroY.update(gyroRadians.y, deltaT);
+    gyroRadians.z = _filterGyroZ.update(gyroRadians.z, deltaT);
+    acc.x = _filterAccX.update(acc.x, deltaT);
+    acc.y = _filterAccY.update(acc.y, deltaT);
     acc.z = _filterAccZ.update(acc.z, deltaT);
 };
