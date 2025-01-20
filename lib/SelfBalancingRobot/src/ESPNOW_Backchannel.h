228ed157377b81afdbb12ac56d86cb6e736e6b68
Martin Budden <mjbudden@gmail.com>
2025-01-20 09:46:41 +0000

    Reordered command packet enums.

--- lib/SelfBalancingRobot/src/ESPNOW_Backchannel.h
+++ lib/SelfBalancingRobot/src/ESPNOW_Backchannel.h
@@ -49,6 +49,6 @@ private:
     SV_Preferences* _preferences {nullptr};
     uint32_t _telemetryID {0};
     uint32_t _backchannelID {0};
-    enum SendType { SEND_NO_DATA, RESET_SCREEN_AND_SEND_NO_DATA, SEND_TICK_INTERVAL_DATA, SEND_PID_DATA, SEND_AHRS_DATA, SEND_MPC_DATA, SEND_RECEIVER_DATA };
+    enum SendType { SEND_NO_DATA=0, RESET_SCREEN_AND_SEND_NO_DATA=1, SEND_TICK_INTERVAL_DATA=2, SEND_AHRS_DATA=3, SEND_RECEIVER_DATA=4, SEND_PID_DATA=5, SEND_MPC_DATA=6 };
     SendType _sendType { RESET_SCREEN_AND_SEND_NO_DATA };
 };
--- lib/StabilizedVehicle/src/CommandPacket.h
+++ lib/StabilizedVehicle/src/CommandPacket.h
@@ -16,7 +16,8 @@ struct CommandPacketReserved {
 
 struct CommandPacketControl {
     enum { TYPE = 1 };
-    enum { NO_ACTION = 0, MOTORS_SWITCH_OFF = 1, MOTORS_SWITCH_ON = 2, RESET = 3,
+    enum { 
+        NO_ACTION = 0, MOTORS_SWITCH_OFF = 1, MOTORS_SWITCH_ON = 2, RESET = 3,
         CONTROL_MODE_0 = 20,
         CONTROL_MODE_1 = 21,
         CONTROL_MODE_2 = 22,
@@ -40,8 +41,8 @@ struct CommandPacketRequestData {
         REQUEST_STOP_SENDING_DATA = 1,
         REQUEST_TICK_INTERVAL_DATA = 2,
         REQUEST_AHRS_DATA = 3,
-        REQUEST_PID_DATA = 4,
-        REQUEST_RECEIVER_DATA = 5,
+        REQUEST_RECEIVER_DATA = 4,
+        REQUEST_PID_DATA = 5,
         REQUEST_MOTOR_CONTROLLER_DATA = 6
     };
     uint32_t id;
@@ -52,14 +53,15 @@ struct CommandPacketRequestData {
 
 struct CommandPacketSetPID {
     enum { TYPE = 3 };
-    enum { NO_ACTION = 0,
-           SET_P = 1,  SET_I = 2,  SET_D = 3,  SET_F = 4,
-           SAVE_P = 5, SAVE_I = 6, SAVE_D = 7, SAVE_F = 8,
-           RESET_PID = 9,
-           SET_SETPOINT = 10,
-           SET_PITCH_BALANCE_ANGLE = 11,
-           SAVE_PITCH_BALANCE_ANGLE = 12
-        };
+    enum {
+        NO_ACTION = 0,
+        SET_P = 1,  SET_I = 2,  SET_D = 3,  SET_F = 4,
+        SAVE_P = 5, SAVE_I = 6, SAVE_D = 7, SAVE_F = 8,
+        RESET_PID = 9,
+        SET_SETPOINT = 10,
+        SET_PITCH_BALANCE_ANGLE = 11,
+        SAVE_PITCH_BALANCE_ANGLE = 12
+    };
     enum { MPC_PITCH_ANGLE=0, MPC_SPEED=1, MPC_YAW_RATE=2, MPC_POSITION=3 };
     enum { FC_ROLL_RATE=0, FC_PITCH_RATE=1, FC_YAW_RATE=2, FC_ROLL_ANGLE=3, FC_PITCH_ANGLE=4 };
     uint32_t id;
