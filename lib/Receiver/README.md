da6809b9fe3947caf0d6d5c5dabb04547b3c3c96
Martin Budden <mjbudden@gmail.com>
2025-04-26 10:54:39 +0100

    Moved Receiver library to separate repository.

--- lib/Receiver/README.md
+++
@@ -1,1 +0,0 @@
-# Receiver Library ![license](https://img.shields.io/badge/license-MIT-green) ![open source](https://badgen.net/badge/open/source/blue?icon=github)
--- lib/Receiver/src/ReceiverTelemetry.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstddef>
-#include <cstdint>
-
-class ReceiverBase;
-
-size_t packTelemetryData_Receiver(uint8_t* telemetryDataPtr, uint32_t id, const ReceiverBase& receiver); // NOLINT(readability-avoid-const-params-in-decls) false positive
