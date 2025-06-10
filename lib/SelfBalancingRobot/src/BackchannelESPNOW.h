a1f060e6ed6e575a325b36fec496eca8b1902509
Martin Budden <mjbudden@gmail.com>
2025-06-10 13:05:51 +0100

    Increased _transmitDataBuffer size for MSP packets.

--- lib/SelfBalancingRobot/src/BackchannelESPNOW.h
+++ lib/SelfBalancingRobot/src/BackchannelESPNOW.h
@@ -34,6 +34,10 @@ protected:
     BackchannelTransceiverESPNOW _transceiver;
     MotorPairController& _motorPairController;
     TelemetryScaleFactors& _telemetryScaleFactors;
-    uint8_t _transmitDataBuffer[256] {}; // > ESP_NOW_MAX_DATA_LEN
-    uint8_t _receivedDataBuffer[256] {}; // > ESP_NOW_MAX_DATA_LEN
+    // If using MSP, then the MSP packets are packed into _transmitDataBuffer by MSP::processOutCommand,
+    // so _transmitDataBuffer must be large enough to hold the larges MSP packet.
+    // If the packet length exceeds ESP_NOW_MAX_DATA_LEN, then it is not sent,
+    // but we don't know its length until we have unpacked it.
+    uint8_t _transmitDataBuffer[512] {};
+    uint8_t _receivedDataBuffer[256] {}; // must be > ESP_NOW_MAX_DATA_LEN
 };
