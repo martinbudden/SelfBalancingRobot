a1f4377b732a5bac2b15e9b508adfecff8a58f25
Martin Budden <mjbudden@gmail.com>
2025-07-01 16:29:02 +0100

    Changed parameter name. Tidied platformio.ini

--- lib/Blackbox/src/BlackboxCallbacksSelfBalancingRobot.h
+++ lib/Blackbox/src/BlackboxCallbacksSelfBalancingRobot.h
@@ -17,8 +17,8 @@ public:
 public:
     virtual void loadSlowStateFromFlightController(blackboxSlowState_t& slowState) override;
     //!! Fill the current state of the blackbox using values read from the flight controller
-    virtual void loadMainStateFromFlightController(blackboxMainState_t& blackboxCurrent) override;
-    virtual void loadMainStateFromFlightController(blackboxMainState_t& blackboxCurrent, const xyz_t& gyroRPS, const xyz_t& gyroRPS_unfiltered, const xyz_t& acc) override;
+    virtual void loadMainStateFromFlightController(blackboxMainState_t& mainState) override;
+    virtual void loadMainStateFromFlightController(blackboxMainState_t& mainState, const xyz_t& gyroRPS, const xyz_t& gyroRPS_unfiltered, const xyz_t& acc) override;
 
     virtual bool isArmed() const override;
     virtual bool isBlackboxRcModeActive() const override;
